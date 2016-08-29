f22bc614c6096b07617310b06f4157103539b6a2
Zorin Alexandr <evol-e@mail.ru>
2016-08-29 17:20:25 +0500

    addув logic scanner

--- src/main/java/ru/technex/networkscanner/model/Scanner.java
+++ src/main/java/ru/technex/networkscanner/model/Scanner.java
@@ -1,13 +1,20 @@
 package ru.technex.networkscanner.model;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scanner {
     private String scanner;
+    private List<String> IPList = new ArrayList<>();
 
     public Scanner() {
     }
 
     public Scanner(String scanner) {
         this.scanner = scanner;
+        checkHosts("192.168.101");
     }
 
     public String getScanner() {
@@ -16,5 +23,22 @@ public class Scanner {
 
     public void setScanner(String scanner) {
         this.scanner = scanner;
+    }
+
+    private String checkHosts(String subnet) {
+        int timeout = 1000;
+
+        for (int i = 1; i < 255; i++) {
+            String host = subnet + "." + i;
+            try {
+                if (InetAddress.getByName(host).isReachable(timeout)) {
+                    IPList.add(host);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return IPList.toString();
     }
 }
